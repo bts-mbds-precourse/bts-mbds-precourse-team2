5f6e8ae143528395b60a34bf993f46bcece8f201
ovdavid28 <43088960+ovdavid28@users.noreply.github.com>
2018-09-17 15:54:44 +0200

    Add files via upload

---
+++ GlobalCLimate.py
@@ -0,0 +1,38 @@
+import csv
+
+file = open('GlobalLandTemperaturesByCity.csv', 'r')
+
+with open('GlobalLandTemperaturesByCity.csv') as csvfile:
+    """sniffer=csv.Sniffer()
+    dialect=sniffer.sniff(csvfile.read(2000))"""
+    readCSV = csv.reader(csvfile,delimiter=',', quoting=csv.QUOTE_NONE)
+    dt = []
+    AverageTemperature = []
+    AverageTemperatureUncertainty = []
+    City = []
+    Country = []
+    Latitude = []
+    Longitude = []
+    i=0
+    for row in readCSV:
+        i+=1
+        date = row[0]
+        print(date)
+        temperaturamedia = row[1]
+        temperaturamedianoexacta = row[2]
+        ciudad = row[3]
+        pais= row[4]
+        latitud = row[5]
+        longitud = row[6]
+
+        dt.append(date)
+        AverageTemperature.append(temperaturamedia)
+        AverageTemperatureUncertainty.append(temperaturamedianoexacta)
+        City.append(ciudad)
+        Country.append(pais)
+        Latitude.append(latitud)
+        Longitude.append(longitud)
+
+    print(dt)
+
+
