909f9e79b379ac0fdfb3ebcf6ecec29a8c6169c8
allisonwalker <36187310+allisonwalker@users.noreply.github.com>
2018-09-25 16:34:00 +0200

    Got some graphs going

--- GlobalCLimate.py
+++ GlobalCLimate.py
@@ -3,15 +3,14 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+import seaborn as sns
 
 data = pd.read_csv('.\DataSet\ByCityShort.csv')
 # print(data)
 
 df = pd.DataFrame(data, columns=['dt', 'AverageTemperature', "City", "Country"])
 rd = df.rename(columns={'dt': "Date", 'AverageTemperature': "AvgTemp", "City": 'City', "Country": 'Country'})
-# print(df)
-
+# print(df) 
 
 rd["Date"] = pd.to_datetime(rd["Date"])
 
@@ -19,41 +18,41 @@ rd["day"] = rd['Date'].map(lambda x: x.day)
 rd["month"] = rd['Date'].map(lambda x: x.month)
 rd["year"] = rd['Date'].map(lambda x: x.year)
 
+
 #to create a list of unique countries in rd
 unique_countries = rd.Country.unique()
 print(unique_countries)
 
-
 #temperature data grouped by country and averaged to a yearly mean
-temp_stats = rd.groupby(['Country','year'])['AvgTemp'].describe()
-
-temp_stats['year', 'Country']=temp_stats.index
-temp_stats.index = range(len(temp_stats))
-
+temp_stats = rd.groupby(['Country', 'year'])['AvgTemp'].describe()
+temp_stats.reset_index(inplace=True)
+temp_stats = pd.DataFrame(temp_stats, columns=['Country', 'year', "mean"])
 print(temp_stats)
-
-print(temp_stats)
-
-#trying to see what happens when plot all data
-temp_stats.plot(x='year', y='AvgTemp', style='-.')
-plt.show()
 
 #attempt to plot each country in a different quadrant
-fig, axes = plt.subplots(nrows=3, ncols=2)
+fig, axes = plt.subplots(nrows=1, ncols=1)
 
-for c in temp_stats(Country):
-    if c == "Denmark":
-        temp_stats.plot(x='year', y='AvgTemp', style='r-.', ax=axes[0, 0])
-    elif c == "Turkey":
-        temp_stats.plot(x='year', y='AvgTemp', style='b-.', ax=axes[0, 1])
-    elif c == "Kazakhstan":
-        temp_stats.plot(x='year', y='AvgTemp', style='g-.', ax=axes[1, 0])
-    elif c == "China":
-        temp_stats.plot(x='year', y='AvgTemp', style='m-.', ax=axes[1, 1])
-    elif c == "Spain":
-        temp_stats.plot(x='year', y='AvgTemp', style='y-.', ax=axes[2, 0])
-    elif c == "Germany":
-        temp_stats.plot(x='year', y='AvgTemp', style='p-.', ax=axes[2, 1])
 
+Denmark = temp_stats.loc[temp_stats['Country'] == 'Denmark']
+Turkey = temp_stats.loc[temp_stats['Country'] == 'Turkey']
+Kazakhstan = temp_stats.loc[temp_stats['Country'] == 'Kazakhstan']
+China = temp_stats.loc[temp_stats['Country'] == 'China']
+Spain = temp_stats.loc[temp_stats['Country'] == 'Spain']
+Germany = temp_stats.loc[temp_stats['Country'] == 'Germany']
+
+"""
+plt.plot('year', 'mean', data = Denmark)
+plt.plot('year', 'mean', data = Turkey)
+plt.plot('year', 'mean', data = Kazakhstan)
+plt.plot('year', 'mean', data = China)
+plt.plot('year', 'mean', data = Spain)
+plt.plot('year', 'mean', data = Germany)
 plt.show()
+"""
 
+sns.lineplot(x = 'year', y = 'mean', data = Denmark)
+sns.lineplot('year', 'mean', data = Turkey)
+sns.lineplot('year', 'mean', data = Kazakhstan)
+sns.lineplot('year', 'mean', data = China)
+sns.lineplot('year', 'mean', data = Spain)
+sns.lineplot('year', 'mean', data = Germany)
