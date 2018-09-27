28d997925b953b9520630e8d26a9ac34c5b13887
allisonwalker <36187310+allisonwalker@users.noreply.github.com>
2018-09-27 16:55:05 +0200

    Add files via upload

--- GlobalCLimate.py
+++ GlobalCLimate.py
@@ -3,157 +3,178 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
-"""
-with open('./DataSet/GlobalLandTemperaturesByCity.csv') as csvfile:
-
-
-file = open('GlobalLandTemperaturesByCity.csv', 'r')
-#This is a testing comment...
-with open('GlobalLandTemperaturesByCity.csv') as csvfile:
-    sniffer=csv.Sniffer()
-    dialect=sniffer.sniff(csvfile.read(2000))
-    readCSV = csv.reader(csvfile,delimiter=',', quoting=csv.QUOTE_NONE)
-    dt = []
-    AverageTemperature = []
-    AverageTemperatureUncertainty = []
-    City = []
-    Country = []
-    Latitude = []
-    Longitude = []
-    i=0
-    for row in readCSV:
-        i+=1
-        date = row[0]
-        print(date)
-        temperaturamedia = row[1]
-        temperaturamedianoexacta = row[2]
-        ciudad = row[3]
-        pais= row[4]
-        latitud = row[5]
-        longitud = row[6]
-
-        dt.append(date)
-        AverageTemperature.append(temperaturamedia)
-        AverageTemperatureUncertainty.append(temperaturamedianoexacta)
-        City.append(ciudad)
-        Country.append(pais)
-        Latitude.append(latitud)
-        Longitude.append(longitud)
-
-    print(dt)
-
-
-"""
-
-
-data = pd.read_csv('./DataSet/ByCityShort.csv')
-#print(data)
-
+data = pd.read_csv('/Users/allison/Desktop/BTS-MBDS/Pre-Course/Project/DataSet/GlobalLandTemperaturesByCity.csv')
+# print(data)
 
 df = pd.DataFrame(data, columns=['dt', 'AverageTemperature', "City", "Country"])
 rd = df.rename(columns={'dt': "Date", 'AverageTemperature': "AvgTemp", "City": 'City', "Country": 'Country'})
-#print(df)
+# print(df)
+
+rd["Date"] = pd.to_datetime(rd["Date"])
+
+rd["day"] = rd['Date'].map(lambda x: x.day)
+rd["month"] = rd['Date'].map(lambda x: x.month)
+rd["year"] = rd['Date'].map(lambda x: x.year)
+rd = rd.loc[rd['year'] > 1850]
+
+
+
+#temperature data grouped by country and averaged to a yearly mean
+temp_stats = rd.groupby(['Country', 'year'])['AvgTemp'].describe()
+temp_stats.reset_index(inplace=True)
+temp_stats = pd.DataFrame(temp_stats, columns=['Country', 'year', 'mean', 'max'])
+print(temp_stats)
+
+
+USA = temp_stats.loc[temp_stats['Country'] == 'United States']
+India = temp_stats.loc[temp_stats['Country'] == 'India']
+Nigeria = temp_stats.loc[temp_stats['Country'] == 'Nigeria']
+China = temp_stats.loc[temp_stats['Country'] == 'China']
+Spain = temp_stats.loc[temp_stats['Country'] == 'Spain']
+Venezuela = temp_stats.loc[temp_stats['Country'] == 'Venezuela']
+
+import seaborn as sns
+
+
+#lineplots means
+sns.lineplot(x = 'year', y = 'mean', data = USA, color = 'Blue', label = 'USA')
+sns.lineplot(x = 'year', y = 'mean', data = India, color = 'Orange', label = 'India')
+sns.lineplot(x = 'year', y = 'mean', data = Nigeria, color = 'Green', label = 'Nigeria')
+sns.lineplot(x = 'year', y = 'mean', data = China, color = 'Red', label = 'China')
+sns.lineplot(x = 'year', y = 'mean', data = Spain, color = 'Yellow', label = 'Spain')
+sns.lineplot(x = 'year', y = 'mean', data = Venezuela, color = 'Purple', label = 'Venezuela')
+sns.despine()
+plt.title('Average Temperatures 1850 to 2013')
+plt.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1, fontsize = 7)
+plt.show()
 
 """
-
-Aachen = rd.loc[rd['City'] == 'Aachen']
-
-Aachen["Date"] = pd.to_datetime(Aachen["Date"])
-
-Aachen["day"] = Aachen['Date'].map(lambda x: x.day)
-Aachen["month"] = Aachen['Date'].map(lambda x: x.month)
-Aachen["year"] = Aachen['Date'].map(lambda x: x.year)
-
-
-def season_finder(month):
-
-    season = []
-    for x in month:
-        if x == 1 or x == 2 or x == 3:
-            season.append("1")
-        elif x == 4 or x == 5 or x == 6:
-            season.append("2")
-        elif x == 7 or x == 8 or x == 9:
-            season.append("3")
-        elif x == 10 or x == 11 or x == 12:
-            season.append("4")
-
-    Aachen["season"] = season
-
-season_finder(Aachen['month'])
-
-print(Aachen)
-
-Aachen_winter = Aachen.loc[Aachen['season'] == '4']
-#Aachen_winter.plot(x='Date', y='AvgTemp', style='.')
-
-Aachen_winter_mean = Aachen_winter.groupby(["year"]).mean()
-
-Aachen_winter_mean["year"]=Aachen_winter_mean.index
-Aachen_winter_mean.index = range(len(Aachen_winter_mean))
-Aachen_winter_mean.plot(x='year', y='AvgTemp', style='-.')
-
-#Aachen_winter_1950 = Aachen_winter.loc[Aachen_winter['year'] == range(1950,2000)]
-#Aachen_winter_1950.plot(x='Date', y='AvgTemp', style='o')
-
+#lineplots max
+sns.lineplot(x = 'year', y = 'max', data = Denmark)
+sns.lineplot(x = 'year', y = 'max', data = Turkey)
+sns.lineplot(x = 'year', y = 'max', data = Kazakhstan)
+sns.lineplot(x = 'year', y = 'max', data = China)
+sns.lineplot(x = 'year', y = 'max', data = Spain)
+sns.lineplot(x = 'year', y = 'max', data = Germany)
+plt.title('Maximum Temperatures 1850 to 2013')
+plt.ylim(0, 30)
 plt.show()
 
 
-"""
-fig, axes = plt.subplots(nrows=2, ncols=2)
-
-def season_plotter(city_string, season_nr_string):
-
-    city_string = rd.loc[rd['City'] == city_string]
-
-    city_string["Date"] = pd.to_datetime(city_string["Date"])
-
-    city_string["day"] = city_string['Date'].map(lambda x: x.day)
-    city_string["month"] = city_string['Date'].map(lambda x: x.month)
-    city_string["year"] = city_string['Date'].map(lambda x: x.year)
-
-    def season_finder(month):
-
-        season = []
-        for x in month:
-            if x == 1 or x == 2 or x == 3:
-                season.append("1") #winter
-            elif x == 4 or x == 5 or x == 6:
-                season.append("2") #spring
-            elif x == 7 or x == 8 or x == 9:
-                season.append("3") #summer
-            elif x == 10 or x == 11 or x == 12:
-                season.append("4") #autumn
-
-        city_string["season"] = season
-
-    season_finder(city_string['month'])
-
-
-    season_dataframe = city_string.loc[city_string['season'] == season_nr_string]
-    #Aachen_winter.plot(x='Date', y='AvgTemp', style='.')
-
-    season_mean_dataframe = season_dataframe.groupby(["year"]).mean()
-
-    season_mean_dataframe["year"] = season_mean_dataframe.index
-    season_mean_dataframe.index = range(len(season_mean_dataframe))
-
-    if season_nr_string == "1":
-        season_mean_dataframe.plot(x='year', y='AvgTemp', style='r-.', ax=axes[0, 0])
-    elif season_nr_string == "2":
-        season_mean_dataframe.plot(x='year', y='AvgTemp', style='b-.', ax=axes[0, 1])
-    elif season_nr_string == "3":
-        season_mean_dataframe.plot(x='year', y='AvgTemp', style='g-.', ax=axes[1, 0])
-    elif season_nr_string == "4":
-        season_mean_dataframe.plot(x='year', y='AvgTemp', style='m-.', ax=axes[1, 1])
-
-
-
-season_string_list = ["1", "2", "3", "4"]
-
-for season_nr in season_string_list:
-    season_plotter("Aachen", season_nr)
-
+#trendline
+sns.regplot(x = 'year', y = 'mean', data = USA, marker=".", x_estimator=np.mean, logx=True, truncate=True, color = 'blue', label = 'USA', scatter_kws={"s": 4})
+sns.regplot(x = 'year', y = 'mean', data = India, marker=".", x_estimator=np.mean, logx=True, truncate=True, color = 'orange', label = 'India', scatter_kws={"s": 4})
+sns.regplot(x = 'year', y = 'mean', data = Nigeria, marker=".", x_estimator=np.mean, logx=True, truncate=True, color = 'green', label = 'Nigeria', scatter_kws={"s": 4})
+sns.regplot(x = 'year', y = 'mean', data = China, marker=".", x_estimator=np.mean, logx=True, truncate=True, color = 'red', label = 'China', scatter_kws={"s": 4})
+sns.regplot(x = 'year', y = 'mean', data = Spain, marker=".", x_estimator=np.mean, logx=True, truncate=True, color = 'yellow', label = 'Spain', scatter_kws={"s": 4})
+sns.regplot(x = 'year', y = 'mean', data = Venezuela,marker=".", x_estimator=np.mean, logx=True, truncate=True, color = 'purple', label = 'Venezuela', scatter_kws={"s": 4})
+sns.despine()
+plt.title('Average Temperatures 1850 to 2013')
+plt.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1, fontsize = 7)
 plt.show()
 
+
+sns.lmplot(x='year', y="mean", data= USA, order=3, palette = "#4daf4a")
+plt.title('USA Average Temperatures 1850 to 2013')
+plt.ylim(12, 17)
+plt.show()
+
+sns.lmplot(x='year', y="mean", data= India, order=3, palette  = "#1f78b4")
+plt.title('India Average Temperatures 1850 to 2013')
+plt.ylim(22, 27)
+plt.show()
+
+sns.lmplot(x='year', y="mean", data= Nigeria, order=3, palette = "#e41a1c")
+plt.title('Nigeria Average Temperatures 1850 to 2013')
+plt.ylim(24, 29)
+plt.show()
+
+sns.lmplot(x='year', y="mean", data= China, order=3, palette = "#7570b3")
+plt.title('China Average Temperatures 1850 to 2013')
+plt.ylim(11, 16)
+plt.show()
+
+sns.lmplot(x='year', y="mean", data= Spain, order=3, palette = "#YlOrBr")
+plt.title('Spain Average Temperatures 1850 to 2013')
+plt.ylim(13, 18)
+plt.show()
+
+sns.lmplot(x='year', y="mean", data= Venezuela, order=3, palette = "Purples")
+plt.title('Venezuela Average Temperatures 1850 to 2013')
+plt.ylim(24, 29)
+plt.show()
+
+"""
+
+
+#grouping by ten year intervals
+rd = rd.groupby(['Country', pd.cut(rd['year'], np.arange(1850, 2010, 10))])['AvgTemp']
+print(list(rd))
+rd = rd.apply(pd.DataFrame)
+#rd = rd.to_frame()
+#rd.reset_index(inplace=True)
+#rd = rd.get_group("Denmark")
+
+print(rd.head())
+
+#rd.boxplot()
+#plt.show()
+USA2 = rd['USA']
+India2 = rd['India']
+Nigeria2 = rd['Nigeria']
+China2 = rd['China']
+Venezuela2 = rd['Venezuela']
+Spain2 = rd['Spain']
+
+
+
+#Boxplots of ten year ranges. One graph per country. 
+sns.boxplot(data = USA2, palette='Blues')
+sns.despine()
+plt.title('United States of America - temperature insight by decade 1850 to 2013')
+plt.tick_params(labelsize=7)
+plt.xticks(rotation=45)
+plt.ylim(-30,30)
+plt.show()
+
+sns.boxplot(data = India2, palette='Oranges')
+sns.despine()
+plt.title('India - temperature insight by decade 1850 to 2013')
+plt.tick_params(labelsize=7)
+plt.xticks(rotation=45)
+plt.ylim(-30,30)
+plt.show()
+
+sns.boxplot(data = Nigeria2, palette='Greens')
+sns.despine()
+plt.title('Nigeria - temperature insight by decade 1850 to 2013')
+plt.tick_params(labelsize=7)
+plt.xticks(rotation=45)
+plt.ylim(-30,30)
+plt.show()
+
+sns.boxplot(data = China2, palette='Reds')
+sns.despine()
+plt.title('China - temperature insight by decade 1850 to 2013')
+plt.tick_params(labelsize=7)
+plt.xticks(rotation=45)
+plt.ylim(-30,30)
+plt.show()
+
+sns.boxplot(data = Spain2, palette='YlOrBr')
+sns.despine()
+plt.title('Spain - temperature insight by decade 1850 to 2013')
+plt.tick_params(labelsize=7)
+plt.xticks(rotation=45)
+plt.ylim(-30,30)
+plt.show()
+
+sns.boxplot(data = Venezuela2, palette = 'Purples')
+sns.despine()
+plt.title('Venezuela - temperature insight by decade 1850 to 2013')
+plt.tick_params(labelsize=7)
+plt.xticks(rotation=45)
+plt.ylim(-30,30)
+plt.show()
